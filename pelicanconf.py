5c840431c8a082e5c7df956f7288fc85a9ba64ad
sumomoe <sumomoe0920@gmail.com>
2026-02-23 17:19:17 +0900

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'moi'
+SITENAME = "moi'sroom"
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'ja'
+
+THEME = 'mytheme'
+
+# Markdownを使う
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.extra': {},
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+
+# 記事URL
+ARTICLE_URL = '{slug}.html'
+ARTICLE_SAVE_AS = '{slug}.html'
+
+# トップページ
+INDEX_SAVE_AS = 'index.html'
+
+# 記事一覧ページ
+DIRECT_TEMPLATES = ['index', 'articles']
+
+#ページURL
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+
+#themeがoutputに現れなかったときに追加したやつ
+THEME = 'C:/Users/sumom/myblog/mytheme'
+STATIC_PATHS = ['static']
+THEME_STATIC_DIR = 'static'
