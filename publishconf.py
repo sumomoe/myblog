3605e1505fa2e9042ba32dfa0107c08ca114a887
sumomoe <sumomoe0920@gmail.com>
2026-02-28 15:06:22 +0900

    Add files via upload

---
+++ publishconf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from pelicanconf import *
+
+SITEURL = 'https://sumomoe.github.io/myblog/profile.html'
+RELATIVE_URLS = False
+
+DELETE_OUTPUT_DIRECTORY = True
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
